c971dd4f6049097bfb86df95c180bb5cf631d229
morexex <ynanaromorexex@gmail.com>
2023-10-12 02:01:19 +0300

    sign in page

---
+++ lib/utils/text.dart
@@ -0,0 +1,12 @@
+class AppText {
+  static final enText = {
+    'welcome_text': 'Welcome',
+    'signIn_text': 'Sign In to Your Account',
+    'registerd_text': 'Already have an account?',
+    'register_text':
+        'You can easily sign up and connect to the Doctors nearby you',
+    'signUp_text': 'Don\'t have an account?',
+    'social_login': 'Or Sign in with social account',
+    'forgot_password': 'Forgot Password?',
+  };
+}
